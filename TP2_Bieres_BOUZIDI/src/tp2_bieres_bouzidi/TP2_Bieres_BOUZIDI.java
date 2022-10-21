32d0d7c44944f3e8754731d2b68a75d295d682d2
Nawfel Bouzidi <nawfel.bouzidi@epfedu.fr>
2022-10-21 16:44:55 +0200

    Fin tp 1

--- TP2_Bieres_BOUZIDI/src/tp2_bieres_bouzidi/TP2_Bieres_BOUZIDI.java
+++ TP2_Bieres_BOUZIDI/src/tp2_bieres_bouzidi/TP2_Bieres_BOUZIDI.java
@@ -21,7 +21,14 @@ public class TP2_Bieres_BOUZIDI {
         BouteilleBiere biere2 = new BouteilleBiere("leffe",6.6,"Abbaye de Leffe") ;
         biere2.ouverte=false;
         biere2.lireEtiquette();
-        System.out.println(uneBiere.Décapsuler());
+        System.out.println(uneBiere);
+    
+       BouteilleBiere HKbienfraiche=new BouteilleBiere("HKbienfraiche",25.0,"Boulaya");
+       BouteilleBiere Jpetema86=new BouteilleBiere("Jpetema86",86,"Charclo");
+       HKbienfraiche.Décapsuler();
+       System.out.println(HKbienfraiche);
+       System.out.println(Jpetema86);
+               
     }
     
 }
