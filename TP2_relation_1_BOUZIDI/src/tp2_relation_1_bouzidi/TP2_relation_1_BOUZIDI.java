8bd64c84ab2d341c901033d639c6972e05bea635
Nawfel Bouzidi <nawfel.bouzidi@epfedu.fr>
2022-10-27 19:24:15 +0200

    Fin exo 4 FIN TP

--- TP2_relation_1_BOUZIDI/src/tp2_relation_1_bouzidi/TP2_relation_1_BOUZIDI.java
+++ TP2_relation_1_BOUZIDI/src/tp2_relation_1_bouzidi/TP2_relation_1_BOUZIDI.java
@@ -23,8 +23,48 @@ public class TP2_relation_1_BOUZIDI {
     System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;  
     
     bob.liste_voitures[0] = uneClio ;
-    bob.nbvoitures = 1 ;
-    uneClio.Proprietaire = Bob ;
+if (bob.ajouter_voiture(uneClio)==true){
+bob.nbvoitures+= 1 ;
+uneClio.Proprietaire = bob ;
+System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
+}
+else{
+    bob.liste_voitures[0]= null;
+    System.out.println("Il y a deja un proprietaire");
+}
+
+bob.liste_voitures[1] = une2008 ;
+if (bob.ajouter_voiture(une2008)==true){
+bob.nbvoitures+= 1 ;
+une2008.Proprietaire = bob ;
+System.out.println("la deuxieme voiture de Bob est " +bob.liste_voitures[1] ) ;
+}
+else{
+   bob.liste_voitures[1]= null;
+   System.out.println("Il y a deja un proprietaire");
+}
+reno.liste_voitures[0] = une2008 ;
+if (reno.ajouter_voiture(une2008)==true){
+reno.nbvoitures += 1 ;
+une2008.Proprietaire = reno ;
+System.out.println("la premiere voiture de reno est " +reno.liste_voitures[0] ) ;
+}
+else{
+    reno.liste_voitures[0]=null;
+    System.out.println("cette voiture a deja un proprietaire");
+}
+reno.liste_voitures[1] = uneMicra ;
+if (reno.ajouter_voiture(uneMicra)==true){
+reno.nbvoitures += 1 ;
+uneMicra.Proprietaire = reno ;
+System.out.println("la deuxieme voiture de reno est " +reno.liste_voitures[1] ) ;
+}
+else{
+    reno.liste_voitures[1]=null;
+    System.out.println("cette voiture a deja un proprietaire");
+}
+
+    
     }
     
 }
